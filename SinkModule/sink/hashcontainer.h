0cce30ef593bb7d15c175d0ed4e2eac766ff9e85
Unknown <mikulasek@live.com>
2017-04-29 22:09:08 +0200

    C implementation of Sink

---
+++ SinkModule/sink/hashcontainer.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+#include "crc64.h"
+
+list* hashtable = NULL;
+
+void hashcontainer_destroy()
+{
+	if (hashtable)
+	{
+		free(hashtable);
+	}
+}
+
+bool hashcontainer_contains(char * value)
+{
+	if (!hashtable)
+		return false;
+
+	unsigned long long crc = crc64(0, (const unsigned char*)value, strlen(value));
+	return sink_list_contains(hashtable, crc);
+}
+
+const unsigned char* hashcontainer_getfield(char* line, int num)
+{
+	const unsigned char* tok;
+	for (tok = (unsigned char *)strtok(line, ",");
+		tok && *tok;
+		tok = (unsigned char *)strtok(NULL, ",\n"))
+	{
+		if (!--num)
+			return tok;
+	}
+	return NULL;
+}
+
+int hashcontainer_init()
+{
+	if (hashtable)
+	{
+		return -1;
+	}
+
+	FILE* dump = fopen("hashtable.dump", "rb");
+	if (dump)
+	{
+		/// Open already sorted and serialized array
+		fseek(dump, 0, SEEK_END);
+		int size = ftell(dump);
+		fseek(dump, 0, SEEK_SET);
+		hashtable = sink_list_init(size / 8);
+		char buffer[8];
+
+		while (fread(buffer, 8, 1, dump) != 0)
+		{
+			unsigned long long* value = (unsigned long long *)buffer;
+			if (sink_list_add(hashtable, *value) != 0)
+				break;
+		}
+		fclose(dump);
+	}
+	else
+	{
+		/// Load CSV and parse it
+		FILE* stream = fopen("/home/ashur/top-5.csv", "r");
+		char line[1024];
+
+		/// Get total number of domains
+		int linecount = 0;
+		while (fgets(line, 1024, stream))
+		{
+			linecount++;
+		}
+
+		/// Prepare hash table
+		hashtable = sink_list_init(linecount);
+		fseek(stream, 0, SEEK_SET);
+		while (fgets(line, 1024, stream))
+		{
+			const unsigned char* value = hashcontainer_getfield(line, 2);
+			unsigned long long crc = crc64(0, value, strlen((const char *)value));
+			sink_list_add(hashtable, crc);
+		}
+		sink_list_sort(hashtable);
+
+		/// Dump table for future use
+		sink_list_dump(hashtable, "hashtable.dump");
+	}
+
+	return 0;
+}
---
+++ SinkModule/sink/list.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>      
+
+typedef struct
+{
+	int capacity;
+	int index;
+	unsigned long long *base;
+} list;
+
+static int sink_list_compare(const void * a, const void * b)
+{
+	const unsigned long long ai = *(const unsigned long long*)a;
+	const unsigned long long bi = *(const unsigned long long*)b;
+
+	if (ai < bi)
+	{
+		return -1;
+	}
+	else if (ai > bi)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+list* sink_list_init(int count)
+{
+	list *result = calloc(1, sizeof(list));
+	result->capacity = count;
+	result->index = 0;
+	result->base = (unsigned long long *)malloc(result->capacity * sizeof(unsigned long long));
+	return result;
+}
+
+void sink_list_destroy(list *item)
+{
+	if (item->base != NULL)
+	{
+		free(item->base);
+		free(item);
+	}
+}
+
+int sink_list_add(list* item, unsigned long long value)
+{
+	if (item->index > item->capacity)
+		return -1;
+
+	/// Silly colision check
+	//for (int i = 0; i < index; i++)
+	//{
+	//	if (base[i] == value)
+	//		return -1;
+	//}
+
+	item->base[item->index] = value;
+	item->index++;
+
+	return 0;
+}
+
+void sink_list_sort(list* item)
+{
+	qsort(item->base, (size_t)item->index, sizeof(unsigned long long), sink_list_compare);
+}
+
+int sink_list_dump(list* item, const char * filename)
+{
+	FILE* file = fopen(filename, "wb");
+	if (file == NULL)
+		return -1;
+
+	int written = fwrite(item->base, sizeof(unsigned long long), item->index, file);
+	if (written != item->index)
+		return -2;
+
+	if (fflush(file) != 0)
+		return -3;
+
+	return fclose(file);
+}
+
+bool sink_list_contains(list* item, unsigned long long value)
+{
+	int lowerbound = 0;
+	int upperbound = item->index;
+	int position;
+
+	position = (lowerbound + upperbound) / 2;
+
+	while ((item->base[position] != value) && (lowerbound <= upperbound))
+	{
+		if (item->base[position] > value)
+		{
+			upperbound = position - 1;
+		}
+		else
+		{
+			lowerbound = position + 1;
+		}
+		position = (lowerbound + upperbound) / 2;
+	}
+
+	return (lowerbound <= upperbound);
+}
