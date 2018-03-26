e6a13912513fc9163c38eaf92766f69911c53f15
Unknown <mikulasek@live.com>
2018-03-26 18:26:46 +0200

    Log to file

--- WhaleboneModule/whalebone/whalebone.h
+++ WhaleboneModule/whalebone/whalebone.h
@@ -15,19 +15,51 @@
 #include "cache_loader.h"
 #include "socket_srv.h"
 
+
+static FILE *log_audit = 0;
+static FILE *log_whalebone = 0;
+
 static __inline void logtosyslog(char *text)
 {
-    openlog("whalebone", LOG_CONS | LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "%s", text);
-    closelog();
+    //openlog("whalebone", LOG_CONS | LOG_PID, LOG_USER);
+    //syslog(LOG_INFO, "%s", text);
+    //closelog();
+    
+    if (log_whalebone == 0) 
+    {
+        log_whalebone = fopen("whalebone.log", "at");
+        if (!log_whalebone) 
+          log_whalebone = fopen("logfile.log", "wt");
+        if (!log_whalebone) 
+        {
+            return;   
+        }
+    }
+    
+    fprintf(log_whalebone, text);
+
     memset(text, 0, strlen(text));
 }
 
 static __inline void logtoaudit(char *text)
 {
-    openlog("whalebone-audit", LOG_CONS | LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "%s", text);
-    closelog();
+    //openlog("whalebone-audit", LOG_CONS | LOG_PID, LOG_USER);
+    //syslog(LOG_INFO, "%s", text);
+    //closelog();
+    
+    if (log_audit == 0) 
+    {
+        log_audit = fopen("audit.log", "at");
+        if (!log_audit) 
+          log_audit = fopen("audit.log", "wt");
+        if (!log_audit) 
+        {
+            return;   
+        }
+    }
+    
+    fprintf(log_audit, text);
+
     memset(text, 0, strlen(text));
 }
  
