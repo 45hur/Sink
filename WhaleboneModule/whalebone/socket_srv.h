39da199d3d3fcf2ad9c0ae7d3eda69f2da6b6cc4
Ashur <mikulasek@live.com>
2018-04-17 18:33:51 +0200

    loopback fix

--- WhaleboneModule/whalebone/socket_srv.h
+++ WhaleboneModule/whalebone/socket_srv.h
@@ -642,7 +642,7 @@ static void* socket_server(void *arg)
 
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	for (int port = 8880; port < 8896; port++)
 	{
 		server.sin_port = htons(port);
