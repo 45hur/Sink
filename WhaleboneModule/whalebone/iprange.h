3c78e06c8dad79103e7f9737c980fdc11a3d004c
Ashur <mikulasek@live.com>
2018-04-25 18:15:25 +0200

    bigendian mess

--- WhaleboneModule/whalebone/iprange.h
+++ WhaleboneModule/whalebone/iprange.h
@@ -8,62 +8,62 @@
 
 struct ipaddr
 {
-  uint16_t family:16;
-  uint32_t ipv4_sin_addr:32;
-  uint64_t ipv6_sin_addr_hi:64;
-  uint64_t ipv6_sin_addr_low:64;
+	uint16_t family : 16;
+	uint32_t ipv4_sin_addr : 32;
+	uint64_t ipv6_sin_addr_hi : 64;
+	uint64_t ipv6_sin_addr_low : 64;
 };
 
-struct ip_addr 
+struct ip_addr
 {
-   uint32_t family;// socket family type
-   unsigned int ipv4_sin_addr;
-   unsigned __int128 ipv6_sin_addr;
+	uint32_t family;// socket family type
+	unsigned int ipv4_sin_addr;
+	unsigned __int128 ipv6_sin_addr;
 };
 
 int is_ip_in_range(const struct ip_addr *ip, const struct ip_addr *from, const struct ip_addr *to)
 {
-   
+
 	int result = 0;
-  if (ip->family != from->family || ip->family != to->family)
-    return result;
-  
+	if (ip->family != from->family || ip->family != to->family)
+		return result;
+
 	switch (ip->family) {
 	case AF_INET: {
-		unsigned int addr_ip = ip->ipv4_sin_addr;
-		unsigned int addr_fr = from->ipv4_sin_addr;
-		unsigned int addr_to = to->ipv4_sin_addr;
-    
-//      printf("%08x => %08x <= %08x\n", 
-//      addr_fr, 
-//     addr_ip, 
-//      addr_to 
-//      );
-    
+		unsigned int addr_ip = __builtin_bswap32(ip->ipv4_sin_addr);
+		unsigned int addr_fr = __builtin_bswap32(from->ipv4_sin_addr);
+		unsigned int addr_to = __builtin_bswap32(to->ipv4_sin_addr);
+
+		//printf("%08x => %08x <= %08x\n",
+		//	addr_fr,
+		//	addr_ip,
+		//	addr_to
+		//);
+
 		result = (addr_ip >= addr_fr) && (addr_ip <= addr_to);
 		break;
 	}
 	case AF_INET6: {
-		 unsigned __int128 addr6_ip = ip->ipv6_sin_addr;
-		 unsigned __int128 addr6_fr = ip->ipv6_sin_addr;
-		 unsigned __int128 addr6_to = ip->ipv6_sin_addr;
+		unsigned __int128 addr6_ip = ip->ipv6_sin_addr;
+		unsigned __int128 addr6_fr = ip->ipv6_sin_addr;
+		unsigned __int128 addr6_to = ip->ipv6_sin_addr;
 
-//      printf("%llx => %llx <= %llx\n", 
-//      (unsigned long long)(addr6_fr& 0xFFFFFFFFFFFFFFFF), 
-//      (unsigned long long)(addr6_ip& 0xFFFFFFFFFFFFFFFF), 
-//      (unsigned long long)(addr6_to& 0xFFFFFFFFFFFFFFFF) 
-//      );
-        
-		  result = memcmp(&addr6_ip, &addr6_fr, 16) >= 0 && memcmp(&addr6_ip, &addr6_to, 16) <= 0;
-  		break;
+		//      printf("%llx => %llx <= %llx\n", 
+		//      (unsigned long long)(addr6_fr& 0xFFFFFFFFFFFFFFFF), 
+		//      (unsigned long long)(addr6_ip& 0xFFFFFFFFFFFFFFFF), 
+		//      (unsigned long long)(addr6_to& 0xFFFFFFFFFFFFFFFF) 
+		//      );
+
+		result = memcmp(&addr6_ip, &addr6_fr, 16) >= 0 && memcmp(&addr6_ip, &addr6_to, 16) <= 0;
+		break;
 	}
 	default:
-  { 
-      printf("wrong ip address\n");
-  		break;
-  }
+	{
+		printf("wrong ip address\n");
+		break;
 	}
-  
+	}
+
 	return result;
 }
 
